ae3b51028bc5da3fce076590af7decdfc785b473
raidnew <raidnew@mail.ru>
2024-11-11 13:26:01 +0300

    Add project files.

---
+++ CSWork22/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSWork22;
+
+internal class Program
+{
+    private static string[] testStrings = new string[]
+    {
+        "",
+        "0",
+        "1",
+        "999999999999999999999999999",
+        "-999999999999999999999999999",
+        "-0",
+        "+123",
+        "-123",
+        "123",
+        "afsda21123",
+        "0OO",
+        "1.234",
+        "-1.234",
+        "-1,234",
+        "1,234",
+        "1,234.324,213,3",
+        "23,431,234.324,213,3",
+        ",431,234.324,213,3",
+        ",431,234.324,213,",
+        "431,234.324,213,",
+        "23`431`234.324`21",
+        "`431`234.324`21",
+        "`431`234.324`",
+        "431`234.324`",
+        "431.234.324`",
+        "431.234,324.324`",
+        "431.234`324.324`"
+    };
+
+    static void Main(string[] args)
+    {
+        test1();
+        
+        Console.ReadKey();
+    }
+
+    static private void showString(string value) => Console.Write($"{value,40}");
+    static private void showString(int value) => showString(value.ToString());
+    static private void showString(double value) => showString(value.ToString());
+
+    static private void test1()
+    {
+        StringToDouble converter = new StringToDouble();
+        foreach (string testString in testStrings)
+        {
+            Console.Write($"{testString, 30} >> ");
+            try
+            {
+                double myd = converter.Convert(testString);
+                showString(myd);
+            }
+            catch (Exception ex)
+            {
+                showString(ex.Message);
+            }
+
+            try
+            {
+                double standart = double.Parse(testString);
+                showString(standart);
+            }
+            catch (Exception ex)
+            {
+                showString(ex.Message);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CSWork22/StringToDouble.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSWork22;
+
+public class StringToDouble
+{
+    public double Convert(string doubleString)
+    {
+        string checkPattern = @"^[+-]?[0-9]+([.,]{1}[0-9]+)?$";
+        bool isMatch = Regex.IsMatch(doubleString, checkPattern);
+        if (!isMatch) throw new FormatException("Can`t format string to double");
+
+        string parsePattern = @"^(?<sign>[+-]?)(?<integer>[0-9]+)([.,]{1}(?<fractal>[0-9]+))?$";
+        Match match = Regex.Match(doubleString, parsePattern);
+
+        bool isPositive = !(match.Groups["sign"].Value == "-");
+        string integerPart = match.Groups["integer"].Value;
+        string fractalPart = match.Groups["fractal"].Value;
+
+        int i;
+        double result = 0, miltiplier;
+
+        int CharToInt(char letter) => letter - '0';
+
+        for (i = (integerPart.Length - 1), miltiplier = 1; i >= 0; i--, miltiplier *= 10)
+            result += CharToInt(integerPart[i]) * miltiplier;
+
+        for (i = 0, miltiplier = 0.1; i < fractalPart.Length; i++, miltiplier /= 10)
+            result += CharToInt(fractalPart[i]) * miltiplier;
+
+        return isPositive ? result : -result;
+    }
+
+}
